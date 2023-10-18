ed495a2ea441b4006e99dfb89c08ae7af063ddcd
hritik kumar mahto <115133698+Hritikkumarmahto@users.noreply.github.com>
2023-10-19 00:03:12 +0530

    Update number recorginition.py

--- number recorginition.py
+++ number recorginition.py
@@ -1,57 +1,101 @@
-import cv2
+import pandas as pd
 import numpy as np
-from sklearn import datasets
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter
+
+data = pd.read_csv("A_Z_Handwritten_Data[1].csv")
+data.head()
+
+for i in range(26):
+    dd = data[data['0']==i].iloc[1]
+    x = dd[1:].values
+    x = x.reshape((28, 28))
+    im = plt.subplot(5, 6, i+1)
+    im.imshow(x, cmap='gray')
+
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+X = data.drop('0',axis = 1) 
+y = data['0']
 
-# Load the digits dataset
-digits = datasets.load_digits()
+X.head()
+y.head()
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2)
 
-# Split the dataset into features and labels
-X = digits.data
-y = digits.target
+X_train = np.reshape(X_train.values, (X_train.shape[0], 28,28))
+X_test = np.reshape(X_test.values, (X_test.shape[0], 28,28))
 
-# Split the dataset into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train.shape
+X_test.shape
 
-# Create a k-Nearest Neighbors classifier
-knn = KNeighborsClassifier(n_neighbors=3)
+word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
+word_dict
 
-# Train the classifier on the training data
-knn.fit(X_train, y_train)
+y_int = np.int0(y) 
+count = np.zeros(26, dtype='int') 
+for i in y_int:
+    count[i] +=1 
 
-# Initialize the camera (change the camera index as needed)
-cap = cv2.VideoCapture(0)
+alphabets = []
+for i in word_dict.values():
+    alphabets.append(i)
+fig, ax = plt.subplots(1,1, figsize=(10,10))
+ax.barh(alphabets, count)
 
-while True:
-    # Capture a frame from the camera
-    ret, frame = cap.read()
+plt.xlabel("Number of elements ")
+plt.ylabel("Alphabets")
+plt.grid()
+plt.show()
 
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from keras.optimizers import SGD, Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.utils import to_categorical
 
-    # Resize the frame to 8x8 pixels
-    small_gray = cv2.resize(gray, (8, 8))
+X_train = X_train.reshape(X_train.shape[0],X_train.shape[1],X_train.shape[2],1)
+print(X_train.shape)
 
-    # Threshold and invert the colors
-    _, small_gray = cv2.threshold(small_gray, 128, 255, cv2.THRESH_BINARY_INV)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2],1)
+print(X_test.shape)
 
-    # Reshape the frame to match the input shape of the classifier
-    input_image = small_gray.reshape(1, -1)
+train_yOHE = to_categorical(Y_train, num_classes = 26, dtype='int')
+test_yOHE = to_categorical(Y_test, num_classes = 26, dtype='int')
 
-    # Predict the digit using the trained classifier
-    predicted_digit = knn.predict(input_image)
 
-    # Display the predicted digit on the frame
-    cv2.putText(frame, str(predicted_digit[0]), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Flatten())
+model.add(Dense(64,activation ="relu"))
+model.add(Dense(128,activation ="relu"))
+model.add(Dense(26,activation ="softmax"))
 
-    # Show the frame
-    cv2.imshow('Digit Recognition', frame)
+model.compile(optimizer = Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(X_train, train_yOHE, epochs=1,  validation_data = (X_test,test_yOHE))
 
-    # Break the loop if the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+print("The validation accuracy is :", history.history['val_accuracy'])
+print("The training accuracy is :", history.history['accuracy'])
+print("The validation loss is :", history.history['val_loss'])
+print("The training loss is :", history.history['loss'])
 
-# Release the camera and close OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+fig, axes = plt.subplots(3,3, figsize=(8,9))
+axes = axes.flatten()
+for i,ax in enumerate(axes):
+    img = np.reshape(X_test[i], (28,28))
+    ax.imshow(img, cmap="Greys")
+    
+    pred = word_dict[np.argmax(test_yOHE[i])]
+    ax.set_title("Prediction: "+pred)
+    ax.grid()
+
+import pickle
+pickle.dump(model,open('model1.pkl','wb'))
+from tensorflow.keras.models import load_model
+import os
+model.save(os.path.join('PycharmProjects','alphabetclassifier.h5'))
+new_model=load_model(os.path.join('AML','alphabetclassifier.h5'))
+
